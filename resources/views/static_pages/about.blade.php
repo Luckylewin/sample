042cd92f32d9541962a0fddbe4e005b92a8df833
lychee <876505905@qq.com>
2017-12-03 15:28:55 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/default')
+@section('title','about page')
+@section('content')
+<h1>about me</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/default')
+@section('title','home page')
+@section('content')
+<h1>hello laravel-by lychee</h1>
+@stop
